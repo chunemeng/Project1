64ee47ec948a3f2cb406b754b0fbb898a239c434
chunemeng <chunemeng@outlook.com>
2024-03-18 12:11:25 +0800

    Init Frontend

---
+++ frontend/src/component/router.jsx
@@ -0,0 +1,19 @@
+import {
+    BrowserRouter,
+    Route,
+    Routes,
+} from "react-router-dom";
+
+
+export default function Router() {
+    return (
+        <BrowserRouter>
+                <Routes>
+                    {/*<Route index="/home" element={<HomePage />} />*/}
+
+                    {/*<Route path="/home" element={<HomePage />} />*/}
+
+                </Routes>
+        </BrowserRouter>
+    );
+}
