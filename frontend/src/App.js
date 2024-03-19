05f9dcb7dfbd224fae6e4c6ee8eea705ab426303
chunemeng <chunemeng@outlook.com>
2024-03-19 19:54:15 +0800

    Add all files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,4 @@
-import logo from './logo.svg';
-import './App.css';
 import Router from "./component/router";
-
-function App() {
-  return (
-    <Router></Router>
-  );
+export default function App() {
+    return <Router></Router>;
 }
-
-export default App;
