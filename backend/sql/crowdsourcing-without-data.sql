c9e20ffa69f87ca717ff9dc2ed252f3f10ce68ff
chunemeng <chunemeng@outlook.com>
2024-04-25 08:21:06 +0800

    store config

---
+++ backend/sql/crowdsourcing-without-data.sql
@@ -0,0 +1,61 @@
+CREATE
+DATABASE IF NOT EXISTS `crowd_sourcing`;
+USE
+crowd_sourcing;
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user`
+(
+    `id`       int UNSIGNED NOT NULL AUTO_INCREMENT,
+    `nickname` varchar(50)        NOT NULL COMMENT '昵称',
+    `username` varchar(50) UNIQUE NOT NULL COMMENT '用户名',
+    `password` varchar(32)        NOT NULL COMMENT '不要明文存储',
+    `email`    varchar(255)       NOT NULL COMMENT '可替换成电话号码',
+    `union_id` int UNSIGNED COMMENT '公会id',
+    `status`   BIT(1) DEFAULT 0 COMMENT '用户权限 true为管理员 false为正常用户 null被禁止登录',
+    PRIMARY KEY (id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8 COMMENT '用户表';
+
+DROP TABLE IF EXISTS `worker`;
+CREATE TABLE `worker`
+(
+    `id`          int UNSIGNED NOT NULL AUTO_INCREMENT,
+    `user_id`     int UNSIGNED NOT NULL COMMENT '用户id/公会管理员id',
+    `nickname`    varchar(50)  NOT NULL COMMENT '昵称',
+    `description` varchar(255) CHARACTER SET utf8mb4 DEFAULT NULL COMMENT '描述',
+    `cover`       varchar(255) NOT NULL COMMENT '图片链接',
+    `status`      boolean      NOT NULL COMMENT 'true for 公会,false for 众包者',
+    PRIMARY KEY (id)
+) ENGINE = InnoDB
+  CHARSET = utf8 COMMENT ='众包者/公会表';
+
+DROP TABLE IF EXISTS `task`;
+CREATE TABLE `task`
+(
+    `id`          int UNSIGNED NOT NULL AUTO_INCREMENT,
+    `description` varchar(255) COMMENT '描述文档链接',
+    `user_id`     int UNSIGNED NOT NULL,
+    `worker_id`   int UNSIGNED NOT NULL,
+    `date`        DATETIME NOT NULL,
+    `end_date`    DATETIME NOT NULL,
+    `category`    bit(4)   NOT NULL COMMENT '任务种类',
+    `status`      bit(1)   NOT NULL DEFAULT 0 COMMENT '设置是否在任务界面展示',
+    PRIMARY KEY (id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8 COMMENT '任务表';
+
+
+DROP TABLE IF EXISTS `order_table`;
+CREATE TABLE `order_table`
+(
+    `id`         int UNSIGNED NOT NULL AUTO_INCREMENT,
+    `user_id`    int UNSIGNED NOT NULL,
+    `worker_id`  int UNSIGNED NOT NULL,
+    `address_id` int UNSIGNED NOT NULL,
+    `task_id`    int UNSIGNED NOT NULL,
+    `end_date`   DATETIME NOT NULL,
+    `status`     bit(2)   NOT NULL DEFAULT 1 COMMENT '任务状态, 进行中, 已结束, 已放弃',
+    PRIMARY KEY (id)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8 COMMENT '订单表';
+
